6fbad11a59d067a9b67a28aa2e5e35d71c23b693
RAL-312 <ral.rayly@gmail.com>
2023-11-06 15:44:13 -0600

    se agrega mi trabajo

---
+++ Inicio.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native';
+
+
+export default class Inicio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+       <View style={toque.fondo}>
+        <View style={toque.fondo2}>
+          <View>
+          <View
+        style={toque.linea1}
+        />
+        <Text style={toque.h1}> VOTA - PLUS</Text>
+        <View
+        style={toque.linea}
+        />
+        <Image
+        style={toque.img1}
+        source={require("./images/Mainfoto.jpeg")}
+        />
+        <Text style={toque.subtitulo}>Ingresa aquí para empezar a votar</Text>
+        <TextInput 
+        style={toque.caja1}
+        placeholder="Email"
+        />
+        <TextInput 
+        style={toque.caja1}
+        placeholder="Password"
+        secureTextEntry={true}
+       />
+       <TouchableOpacity style={toque.ingresar}>
+        <Text style={toque.subtitulo2}>Ingresar</Text>
+       </TouchableOpacity>
+
+      <Text style={toque.subtitulo}>Si eres administrador{'\n'} Ingresa aquí con tu cuenta única</Text>
+      <TouchableOpacity><Text style={toque.subtituloAqui}>Aquí</Text></TouchableOpacity>
+        </View>
+        </View>
+       </View>
+      </View>
+    );
+  }
+}
+
+const toque = StyleSheet.create({
+  fondo: {
+   width: "100%",
+   height: "100%",
+   backgroundColor: "#962424"
+  },
+
+ fondo2: {
+  width: "95%",
+  height:"95%",
+  borderColor: "#fca50d",
+  backgroundColor: "white",
+  borderRadius: 1,
+  marginLeft: 8,
+  marginTop: "5%",
+ },
+
+ h1: {
+  fontFamily: 'OpenSans-VariableFont_wdth,wght',
+  fontSize: 50,
+  color: "black",
+  textAlign:"center",
+  marginRight: "4%",
+  marginTop: "12%"
+ },
+
+ img1: {
+  width: 250,
+  height: 200,
+  borderRadius: 10,
+  marginLeft: "19%",
+  marginTop: 10,
+ },
+
+ linea:{
+  width: "80%",
+  height: 2,
+  backgroundColor: 'black',
+  marginLeft: "11%",
+ },
+
+ subtitulo:{
+  fontFamily:"OpenSans-VariableFont_wdth,wght",
+  fontSize: 16,
+  textAlign:"center",
+  marginTop: 30,
+  color: "black",
+ },
+
+ subtituloAqui:{
+  fontFamily:"OpenSans-VariableFont_wdth,wght",
+  fontSize: 16,
+  textAlign:"center",
+  marginTop: 10,
+  color: "blue",
+  textDecorationLine: "underline",
+ },
+
+ subtitulo2:{
+  fontFamily:"OpenSans-VariableFont_wdth,wght",
+  fontSize: 16,
+  fontWeight: "bold",
+  textAlign:"center",
+  marginTop: 6,
+  color: "white",
+ },
+
+ caja1:{
+  width: 240,
+  height: 40,
+  borderWidth: 1,
+  borderColor: "black",
+  backgroundColor: "white",
+  marginTop: 20,
+  marginLeft: "20%",
+  borderRadius: 3,
+ },
+
+ ingresar:{
+  width: 100,
+  height: 40,
+  borderWidth: 1,
+  marginLeft: 150,
+  marginTop: 15,
+  borderRadius: 10,
+  backgroundColor: "#3d3c3b"
+ },
+ 
+
+}); 
---
+++ Navegación.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';//Navegación
+import { createNativeStackNavigator } from '@react-navigation/native-stack';//Navegación
+import Inicio from './Inicio';
+import Admin from './Admin';
+export default class Navegación extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    const Stack = createNativeStackNavigator(); 
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          
+        <Stack.Screen name="Inicio" component={Inicio} options={{headerShown:false}} />
+        <Stack.Screen name="Admin" component={Admin} options={{headerShown:false}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
